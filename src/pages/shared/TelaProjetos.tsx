2aac570b7b9485ef58e22322a91a43ed6b16fd19
davi <fdavi07@gmail.com>
2026-06-14 02:49:55 -0300

    Várias fita mudada

--- src/pages/shared/TelaProjetos.tsx
+++ src/pages/shared/TelaProjetos.tsx
@@ -309,7 +309,6 @@ export default function TelaProjetos({
                             <path d="M3 7a2 2 0 012-2h4l2 2h8a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V7z" />
                           </svg>
                         </div>
-                        <span className="card-badge">ERS</span>
                       </div>
                       <div className="card-name">{p.nome}</div>
                       <div className="card-client">
